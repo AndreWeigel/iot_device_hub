c5abb4d84f5f252cae70fccacb986a18d3d34ce3
andreweigel <aweigel.2000@gmail.com>
2025-05-22 14:50:44 -0300

    User model

---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, nullable=True)
+    created_at = Column(DateTime, nullable=False)
