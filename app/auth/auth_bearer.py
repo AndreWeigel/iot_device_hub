4d8aa729beef26097246f251c424db44dc1085ae
andreweigel <aweigel.2000@gmail.com>
2025-06-01 17:16:11 -0300

    correct to async db session

--- app/auth/auth_bearer.py
+++ app/auth/auth_bearer.py
@@ -1,15 +1,16 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 from jose import JWTError, jwt
 from app.schemas.user import TokenData, UserInDB
 from app.auth.auth_handler import SECRET_KEY, ALGORITHM
 from app.services.user_service import UserService
-from sqlalchemy.orm import Session
 from app.db.deps import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail="Could not validate credentials",
                                           headers={"WWW-Authenticate": "Bearer"})
